ed8d647405bdf5fa1da2c6b93d936474fcb02e39
fazha.ivanda <fazha.ivanda@advancegroup.com>
2026-05-28 12:49:47 +0700

    feat: refactor and finalize db migrations

--- migrations/004_add_llm_fields_to_trades.up.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE trades ADD COLUMN IF NOT EXISTS llm_confidence INT;
-ALTER TABLE trades ADD COLUMN IF NOT EXISTS llm_entry_mode VARCHAR(16);
-ALTER TABLE trades ADD COLUMN IF NOT EXISTS llm_entry_reasons TEXT[];
-ALTER TABLE trades ADD COLUMN IF NOT EXISTS llm_warnings TEXT[];
